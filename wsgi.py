d429c7d689e29999b6fafbdca0c9a77cadff8aae
Remus Buzatu <r.buzatu90@gmail.com>
2019-11-21 09:07:56 +0400

    Echo hostname

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import socket
 application = Flask(__name__)
 
-@application.route("/")
+@application.route("/hello")
 def hello():
     return "Hello World!"
+
+@application.route("/")
+def return_hostname():
+    return "Hostname {} \n".format(socket.gethostname())
 
 if __name__ == "__main__":
     application.run()
